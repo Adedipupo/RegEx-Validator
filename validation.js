9d585f5b262096c642a69393f5d964637ef776a2
Adedipupo <bayonleolaluwa@gmail.com>
2020-08-10 02:30:55 +0100

    dob

--- validation.js
+++ validation.js
@@ -2,4 +2,10 @@ const checkInput = (fullName,email,Username,dateOfBirth,website,streetAddress) =
     let fullNamePattern = / \D+ [a - z]\s$/gi
 
     let emailPattern = / \w{3,}.@\D.+\D $/gi
+
+    let Username =  / ^ [a - z]([0 - 9][0 - 9] +| [a - z] +\d *)$/ i 
+
+    let dateOfBirth = / \d{2}-\d{2}-\d{4} /
+
+    let website
 }
