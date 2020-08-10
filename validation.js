6aa9b09392878f68adaf4eabdc05c048297dceae
Adedipupo <bayonleolaluwa@gmail.com>
2020-08-10 02:25:32 +0100

    fullnamepattern

--- validation.js
+++ validation.js
@@ -1,3 +1,5 @@
 const checkInput = (fullName,email,Username,dateOfBirth,website,streetAddress) = {
-    let fullNamePattern = / \D+ [a - z]\s/
+    let fullNamePattern = / \D+ [a - z]\s$/gi
+
+    let emailPattern = / \w{3,}.@\D.+\D $/gi
 }
