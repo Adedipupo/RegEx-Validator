48963bc9daf9c10e07c2ce9e6352d3abd8720153
Adedipupo <bayonleolaluwa@gmail.com>
2020-08-10 02:59:01 +0100

    regex

--- validation.js
+++ validation.js
@@ -3,11 +3,11 @@ const checkInput = (fullName,email,Username,dateOfBirth,website,streetAddress) =
 
     let emailPattern = / \w{3,}.@\D.+\D $/gi
 
-    let Username =  / ^ [a - z]([0 - 9][0 - 9] +| [a - z] +\d *)$/i
+    let UsernamePattern =  / ^ [a - z]([0 - 9][0 - 9] +| [a - z] +\d *)$/i
 
-    let dateOfBirth = / \d{2}-\d{2}-\d{4} $/
+    let dateOfBirthPattern = / \d{2}-\d{2}-\d{4} $/
 
-    let website = / (https:)?(www).\w{2,}.\D{3} $/g
+    let websitePattern = / (https:)?(www).\w{2,}.\D{3} $/g
 
-    let streetAddress = /\w+(\s\w+){2,}$/gi
+    let streetAddressPattern = /\w+(\s\w+){2,}$/gi
 }
